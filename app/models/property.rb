0a4d07282dedde162ba74a1c42083ef49941cb1f
DarthCharles <carlos.contreras@points.com>
2019-04-01 14:47:05 -0700

    :pencil: Added acts_as_mappable to property model & fixtures

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,19 @@
 class Property < ApplicationRecord
+  acts_as_mappable
 
   validates :city, :zip_code, presence: true
+
+
+  def as_json
+    {
+      type:         property_type,
+      house_number: house_number,
+      street:       street,
+      city:         city,
+      zip_code:     zip_code,
+      lat:          lat,
+      lng:          lng,
+      price:        price
+    }
+  end
 end
